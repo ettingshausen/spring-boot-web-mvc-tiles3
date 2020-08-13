42c32f8fddfeea65d62d9bd18e6813b6c1405d21
ettingshausen <ettingshausen@outlook.com>
2020-08-13 17:40:58 +0800

    Simplify main class annotation

--- src/main/java/com/mvmlabs/springboot/Application.java
+++ src/main/java/com/mvmlabs/springboot/Application.java
@@ -2,6 +2,7 @@ package com.mvmlabs.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * 
  * @author Mark Meany
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
